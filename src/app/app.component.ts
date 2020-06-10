cc695161b52ccb08b8211d84619cb82f82b04d2f
Shimoni <gal.shimoni@hp.com>
2020-06-10 23:02:26 +0300

    First commit in project

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import {users} from './data/data'
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'COVID';
+  covidUsers = users;
+  sickPeople = 0;
+  quarantinedPeople = 0;
+  healthyPeople = 0;
+// x;
+// y;
+//   imgs = ['https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+// 'https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736877_1280.jpg',
+// 'https://cdn.pixabay.com/photo/2014/05/07/06/44/animal-339400_1280.jpg'];
+
+constructor(){
+
+  for(let i=0;i<this.covidUsers.length;i++){
+    if(this.covidUsers[i].hasCovid){
+      this.sickPeople += 1;
+    }
+    else{
+      this.healthyPeople += 1;
+    }
+    if(this.covidUsers[i].isolated){
+      this.quarantinedPeople += 1;
+    }
+  }
+  
+}
+
+  // move(e: MouseEvent){
+  //   this.x = e.clientX +50;
+  //   this.y = e.clientY+50;
+  //  // console.log(event);
+  //  // this.isLoggedIn = !this.isLoggedIn;
+  // }
+}
---
+++ src/app/data/data.ts
@@ -0,0 +1,25 @@
+
+
+ export let users = [
+    {fname: 'Dan',lname: 'Mark',hasCovid: true,isolated: false,isAdmin: false},
+    {fname: 'Lin',lname: 'Horz',hasCovid: true,isolated: false,isAdmin: true},
+    {fname: 'Beni',lname: 'Men',hasCovid: false,isolated: true,isAdmin: false},
+    {fname: 'Adam',lname: 'Brodie',hasCovid: false,isolated: true,isAdmin: false},
+    {fname: 'Arik',lname: 'Raco',hasCovid: true,isolated: true,isAdmin: false},
+    {fname: 'Lee',lname: 'Bell',hasCovid: false,isolated: true,isAdmin: false},
+    {fname: 'Yoni',lname: 'Sten',hasCovid: false,isolated: false,isAdmin: false},
+    {fname: 'Gilad',lname: 'Raben',hasCovid: false,isolated: false,isAdmin: false},
+    {fname: 'Hana',lname: 'Eaker',hasCovid: true,isolated: false,isAdmin: false},
+    {fname: 'Hagay',lname: 'Fabiani',hasCovid: false,isolated: false,isAdmin: false},
+    {fname: 'Lin',lname: 'Martinez',hasCovid: false,isolated: false,isAdmin: false},
+    {fname: 'David',lname: 'Davis',hasCovid: false,isolated: true,isAdmin: false},
+    {fname: 'Jane',lname: 'Miller',hasCovid: false,isolated: false,isAdmin: false},
+    {fname: 'Bob',lname: 'Garcia',hasCovid: false,isolated: true,isAdmin: false},
+    {fname: 'Arik',lname: 'Williams',hasCovid: false,isolated: true,isAdmin: false},
+    {fname: 'Din',lname: 'Smith',hasCovid: true,isolated: false,isAdmin: false},
+    {fname: 'Efrat',lname: 'Man',hasCovid: false,isolated: false,isAdmin: false},
+    {fname: 'Nir',lname: 'Biren',hasCovid: true,isolated: false,isAdmin: false},
+    {fname: 'Shir',lname: 'Hendrix',hasCovid: false,isolated: false,isAsmin: false},
+    {fname: 'Mirit',lname: 'Joplin',hasCovid: true,isolated: false,isAdmin: false}      
+    
+  ];
